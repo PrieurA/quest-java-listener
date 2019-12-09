d27135249ba39c25f16a67bb258a20dc9201024a
PrieurA <alexandre-prieur_student2019@wilder.school>
2019-12-09 13:56:00 +0100

    done

--- Main.java
+++ Main.java
@@ -2,11 +2,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Bank account = new Bank(1000);
-
         // TODO : create and use a TransactionListener interface with onComplete method
-        Transactions.withdraw(account, 100);
 
-        System.out.printf("Your balance is: %d%n", account.getTotal());
+        Bank account = new Bank(1000);
+        int value = 100;
+
+        Transactions.withdraw(account, value, new TransactionListener() {
+            @Override
+            public void onComplete(Bank account) {
+                System.out.printf("Your balance is: %d%n", account.getTotal());
+            }
+        });
+
     }
 }
--- Transactions.java
+++ Transactions.java
@@ -1,14 +1,15 @@
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class Transactions {
+public class Transactions{
 
-    public static void withdraw(Bank account, int value) {
+    public static void withdraw(Bank account, int value, TransactionListener event) {
         int time = (int) (Math.random() * ((1000 - 500) + 1)) + 500;
         new Timer().schedule(new TimerTask() {
             @Override
             public void run() {
-                account.setTotal(account.getTotal() - value);
+                Bank a = new Bank(account.getTotal() - value);
+                event.onComplete(a);
             }
         }, time);
     }
